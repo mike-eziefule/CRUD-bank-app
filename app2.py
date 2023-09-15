db24070a5c7106acc7efe355a8cbc65803225fc7
mike-eziefule <mike.eziefule@gmail.com>
2023-09-15 17:02:18 +0100

    Created different db to save Savings and current type

--- app2.py
+++ app2.py
@@ -7,7 +7,7 @@ import random
 app = FastAPI()
 
 class User(BaseModel):
-    username: str
+    
     firstname: str
     lastname: str
     Telephone: str
@@ -15,14 +15,20 @@ class User(BaseModel):
     password: str
     gender: str = None
     age: int = None
-    location: str= None
+    Address: str= None
     account_type: str = "Saving Account"
     
 class createUser(User):
     id: UUID
     account_num: int
-
-database:dict[str, createUser] = {}
+    username: str
+    
+# class Database(BaseModel):
+#     database:list[createUser]
+    
+SavingsDB:dict[str, createUser] = {} # dictionary to store saving accounts
+CurrentDB:dict[str, createUser] = {} # dictionary to store current
+database:dict[str, createUser] = {} # dictionary to store
 
 def gen_account_number():
     random_numbers = [901]
@@ -37,22 +43,36 @@ def home():
 
 @app.get("/Users")
 def all_users():
+    if database == {}:
+        return {"message":"List is empty"}
     return database
 
 @app.get("/Users/{account_num}")
-def user(account_num:int):
-    user = database.get(account_num)
-    if user:
-        return user
-    return {"message":"user not found"}
+def get_Account_info(account_num):
+    user = database.get(int(account_num))
+
+    if not user:
+        return {"message":"user not found"}
+    
+    return user
 
 @app.post("/Create_users")
-def new_User(user_in: User):    
+def new_User(user_in: User):
     new_user = createUser(
         id = UUID(int=(len(database)+1)),
-        account_num = gen_account_number(),
+        username = user_in.firstname.capitalize()+'.'+user_in.lastname[0].upper(), #generate a user name from First and Last names
+        account_num = gen_account_number(), #generates a unique account number starting with 901
         **user_in.dict()
     )
-    database[new_user.username] = new_user
+    database[new_user.account_num] = new_user
     
-    return new_user    
+    if new_user.account_type == "Savings Account":
+        SavingsDB[new_user.account_num] = new_user
+        return {"message": "Saved in Savings Account DB",
+            "account Information": new_user}
+    elif new_user.account_type == "Current Account":
+        CurrentDB[new_user.account_num] = new_user
+        return {"message": "Saved in Current Account DB",
+        "account Information": new_user}
+    else:
+        return "Account type not found"
